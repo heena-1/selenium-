7a04de9991aa36dd8e1c90010af4c2aa7bbd423d
W7 <W7@W7-PC>
2021-10-06 15:34:38 +0530

    by user..

---
+++ git22/src/gitprograms/Demo12.java
@@ -0,0 +1,14 @@
+package gitprograms;
+
+public class Demo12 {
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+public static void main(String[] args) {
+	System.out.println("hi");
+	System.out.println("hello");
+}
+}
+
+
